68ef0c3e13cdea08c25d1a44db0b9efff56e6627
mwtechnican <moritz.wiedmann@outlook.de>
2021-11-26 22:15:30 +0100

    made script using parameters

--- scripts/load_checkmk_image.sh
+++ scripts/load_checkmk_image.sh
@@ -2,9 +2,12 @@
 # Download: https://checkmk.com/download?edition=cfe&version=stable&dist=docker
 # Guide: https://docs.checkmk.com/latest/en/introduction_docker.html
 # simply change filename to the version needed
-CHECKMKVERSION=2.0.0p15
-CHECKMKFILENAME=check-mk-free-docker-$CHECKMKVERSION.tar.gz
+
+CHECKMK_EDITION=$1
+CHECKMK_VERSION=$2
+
+FILENAME=$CHECKMK_EDITION-$CHECKMK_VERSION.tar.gz
 
 sudo mkdir ./images
-sudo wget https://download.checkmk.com/checkmk/2.0.0p15/$CHECKMKFILENAME -O ./images/$CHECKMKFILENAME
-sudo docker load -i ./images/$CHECKMKFILENAME
+sudo wget https://download.checkmk.com/checkmk/2.0.0p15/$FILENAME -O ./images/$FILENAME
+sudo docker load -i ./images/$FILENAME
