cc39e285653efee72d18a4cf7a421571496aeeeb
mwtechnican <moritz.wiedmann@outlook.de>
2021-11-26 22:14:50 +0100

    added scripts

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 . .env
 
 # install docker and docker-compose
-install_requirements.sh
+bash ./scripts/install_requirements.sh
 
-# create containerdata folder
-sudo mkdir ./containerdata
+# load checkmk image and import to docker
+bash ./scripts/load_checkmk_image.sh $CHECKMK_EDITION $CHECKMK_VERSION
+
+# create network
+bash ./scripts/create_external_networks.sh
 
 # secrets should not be empty
 sudo docker-compose -f docker-compose.yml -p home-lab pull
