bb4c1c54d8497a048d0aa71a69b8bddb6ed7b837
mwtechnican <moritz.wiedmann@outlook.de>
2021-11-26 22:55:30 +0100

    renaming

--- initial-setup.sh
+++ initial-setup.sh
@@ -11,13 +11,6 @@ bash ./scripts/load_checkmk_image.sh $CHECKMK_EDITION $CHECKMK_VERSION
 echo -e "\e[32mcreate docker network\e[0m"
 bash ./scripts/create_external_networks.sh
 
-echo -e "\e[32msecrets should not be empty, are they?\e[0m"
-read -e -p "y/n" SECRETS_EMPTY
-
-if [ $SECRETS_EMPTY -eq "n" ]
-then
-    sudo docker-compose -f docker-compose.yml -p home-lab pull
-    sudo docker-compose -f docker-compose.yml -p home-lab up -d
-else
-   echo "please fix empty secret files"
-fi
+echo -e "\e[32msecrets should not be empty\e[0m"
+sudo docker-compose -f docker-compose.yml -p home-lab pull
+sudo docker-compose -f docker-compose.yml -p home-lab up -d
