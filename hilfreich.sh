4d56bbdfca87533c4ccbe5fb2e47c9bb9012aa2d
mwtechnican <moritz.wiedmann@outlook.de>
2021-11-26 22:14:35 +0100

    removed

--- hilfreich.sh
+++
@@ -1,6 +0,0 @@
-sudo docker-compose -f /containerdata/docker-compose.yaml up -d
-
-sudo docker-compose -f /containerdata/docker-compose.yaml down
-rm /containerdata -rf
-
-sudo docker rm traefik pihole bitwarden portainer checkmk -f
