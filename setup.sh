37090c7bd376de208022b1e27264cdc5269f5f30
mwtechnican <moritz.wiedmann@outlook.de>
2021-11-26 22:29:04 +0100

    added color to echo

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# load variables
+echo -e "\e[32mloading variables '.env'\e[0m"
 . .env
 
-# install docker and docker-compose
+echo -e "\e[32minstall docker and docker-compose\e[0m"
 bash ./scripts/install_requirements.sh
 
-# load checkmk image and import to docker
+echo -e "\e[32mload checkmk image and import to docker\e[0m"
 bash ./scripts/load_checkmk_image.sh $CHECKMK_EDITION $CHECKMK_VERSION
 
-# create network
+echo -e "\e[32mcreate docker network\e[0m"
 bash ./scripts/create_external_networks.sh
 
-# secrets should not be empty
+echo -e "\e[32msecrets should not be empty\e[0m"
 sudo docker-compose -f docker-compose.yml -p home-lab pull
 sudo docker-compose -f docker-compose.yml -p home-lab up -d
