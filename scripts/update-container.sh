73a15cc1fc885d943b25b838ebf6b62628d1392c
mwtechnican <moritz.wiedmann@outlook.de>
2021-12-19 22:18:34 +0100

    fixed naming

--- scripts/update-container.sh
+++ scripts/update-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker-compose -f /homelab/docker-compose.yaml -p home-lab pull
-docker-compose -f /homelab/docker-compose.yaml -p home-lab up -d
+docker-compose -f /homelab/docker-compose.yml -p home-lab pull
+docker-compose -f /homelab/docker-compose.yml -p home-lab up -d
 docker image prune -f
 echo ""
 echo ""
