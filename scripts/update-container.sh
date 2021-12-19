f47a339eec8a4ee6788e66fa6610c0e71b6a381d
mwtechnican <moritz.wiedmann@outlook.de>
2021-12-19 21:30:51 +0100

    removed sudo + changed path

--- scripts/update-container.sh
+++ scripts/update-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo docker-compose -f /containerdata/docker-compose.yaml -p home-lab pull
-sudo docker-compose -f /containerdata/docker-compose.yaml -p home-lab up -d
-sudo docker image prune -f
+docker-compose -f /homelab/docker-compose.yaml -p home-lab pull
+docker-compose -f /homelab/docker-compose.yaml -p home-lab up -d
+docker image prune -f
 echo ""
 echo ""
