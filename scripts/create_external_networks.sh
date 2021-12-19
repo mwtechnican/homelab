c173e39913159a4b4eca205f3c5014c2d1f684fc
mwtechnican <moritz.wiedmann@outlook.de>
2021-12-19 21:30:40 +0100

    removed sudo

--- scripts/create_external_networks.sh
+++ scripts/create_external_networks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this is used to create the docker network 
 # create networks
-sudo docker network create --driver=bridge --subnet=172.18.0.0/24 proxynet
-sudo docker network create --driver=bridge --subnet=172.19.0.0/24 socketproxynet
+docker network create --driver=bridge --subnet=172.18.0.0/24 proxynet
+docker network create --driver=bridge --subnet=172.19.0.0/24 socketproxynet
 echo ""
 echo ""
