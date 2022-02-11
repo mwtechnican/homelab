f0a9266f688904e48a67bdfe591ca6fc3bd7875e
mwtechnican <moritz.wiedmann@outlook.de>
2022-02-11 19:52:30 +0100

    updated secret usage

--- initial-setup.sh
+++ initial-setup.sh
@@ -3,46 +3,52 @@ echo -e "\e[32mloading variables '.env'\e[0m"
 . .env
 
 echo -e "\e[32mnow enter the secrets which should be used for the containers\e[0m"
-read -sp "Enter bitwarden_admin_token: " bitwarden_admin_token
-read -sp "Enter ionos_api_key: " ionos_api_key
-read -s "Enter nextcloud_admin_user: " nextcloud_admin_user
-read -sp "Enter nextcloud_admin_password: " nextcloud_admin_password
-read -s "Enter nextcloud_postgres_db: " nextcloud_postgres_db
-read -s "Enter nextcloud_postgres_user: " nextcloud_postgres_user
-read -sp "Enter nextcloud_postgres_password: " nextcloud_postgres_password
-read -s "Enter smtp_host: " smtp_host
-read -s "Enter smtp_user: " smtp_user
-read -s "Enter smtp_password: " smtp_password
 
+read -p -s "Enter bitwarden_admin_token: " bitwarden_admin_token
 echo $bitwarden_admin_token >> ./secrets/bitwarden_admin_token
 unset bitwarden_admin_token
 
-read $ionos_api_key>> ./secrets/ionos_api_key
+read -p -s "Enter ionos_api_key: " ionos_api_key
+echo $ionos_api_key >> ./secrets/ionos_api_key
 unset ionos_api_key
 
-read $nextcloud_admin_password>> ./secrets/nextcloud_admin_password
-unset nextcloud_admin_password
-
-read $nextcloud_admin_user>> ./secrets/nextcloud_admin_user
+read -p "Enter nextcloud_admin_user [admin]: " nextcloud_admin_user
+nextcloud_admin_user=${nextcloud_admin_user:-admin}
+echo $nextcloud_admin_user >> ./secrets/nextcloud_admin_user
 unset nextcloud_admin_user
 
-read $nextcloud_postgres_db>> ./secrets/nextcloud_postgres_db
+read -p -s "Enter nextcloud_admin_password: " nextcloud_admin_password
+echo $nextcloud_admin_password >> ./secrets/nextcloud_admin_password
+unset nextcloud_admin_password
+
+read -p "Enter nextcloud_postgres_db [nextcloud-db]: " nextcloud_postgres_db
+nextcloud_postgres_db=${nextcloud_postgres_db:-'nextcloud-db'}
+echo $nextcloud_postgres_db >> ./secrets/nextcloud_postgres_db
 unset nextcloud_postgres_db
 
-read $nextcloud_postgres_password>> ./secrets/nextcloud_postgres_password
-unset nextcloud_postgres_password
-
-read $nextcloud_postgres_user>> ./secrets/nextcloud_postgres_user
+read -p "Enter nextcloud_postgres_user [nextcloud]: " nextcloud_postgres_user
+nextcloud_postgres_user=${nextcloud_postgres_user:-'nextcloud'}
+echo $nextcloud_postgres_user >> ./secrets/nextcloud_postgres_user
 unset nextcloud_postgres_user
 
-read $smtp_host>> ./secrets/smtp_host
+read -p -s "Enter nextcloud_postgres_password: " nextcloud_postgres_password
+echo $nextcloud_postgres_password >> ./secrets/nextcloud_postgres_password
+unset nextcloud_postgres_password
+
+read -p "Enter smtp_host [smtp.ionos.de]: " smtp_host
+smtp_host=${smtp_host:-smtp.ionos.de}
+echo $smtp_host >> ./secrets/smtp_host
 unset smtp_host
 
-read $smtp_password>> ./secrets/smtp_password
+read -p "Enter smtp_user: " smtp_user
+echo $smtp_user >> ./secrets/smtp_user
+unset smtp_user
+
+read -p -s "Enter smtp_password: " smtp_password
+echo $smtp_password >> ./secrets/smtp_password
 unset smtp_password
 
-read $smtp_user>> ./secrets/smtp_user
-unset smtp_user
+####################################################################################
 
 echo -e "\e[32minstall docker and docker-compose\e[0m"
 bash ./scripts/install_requirements.sh
