0d09856242a5dc9b732aa385ec4bdbc363bb9b69
mwtechnican <moritz.wiedmann@outlook.de>
2022-02-11 19:18:51 +0100

    moved to root

--- update-container.sh
+++ update-container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker-compose -f /homelab/docker-compose.yml -p home-lab pull
 docker-compose -f /homelab/docker-compose.yml -p home-lab up -d
+docker-compose -f docker-compose-nextcloud.yml -p home-nextcloud pull
+docker-compose -f docker-compose-nextcloud.yml -p home-nextcloud up -d
 docker image prune -f
 echo ""
 echo ""
