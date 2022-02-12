1ec8cb76ff23db2161e5c07f20d775ad9189a693
mwtechnican <moritz.wiedmann@outlook.de>
2022-02-12 15:50:12 +0100

    changed to one dockernet

--- scripts/create_external_networks.sh
+++ scripts/create_external_networks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # this is used to create the docker network 
 # create networks
-docker network create --driver=bridge --subnet=172.18.0.0/24 proxynet
+docker network create --driver=bridge --subnet=172.18.0.0/24 dockernet
 echo ""
 echo ""
