6b5c82f72575aac04e7eb95f473f59011fd1ff79
mwtechnican <moritz.wiedmann@outlook.de>
2022-02-13 23:26:53 +0100

    added ignore for checkmk failure

--- update-container.sh
+++ update-container.sh
@@ -9,11 +9,11 @@ echo -e "${blue}loading checkmk image${reset}"
 bash scripts/get_checkmk_image.sh $checkmk_edition $checkmk_version
 
 echo -e "${blue}updating containers used in stack 'home-lab'${reset}"
-docker-compose -f docker-compose.yml -p home-lab pull
+docker-compose -f docker-compose.yml -p home-lab pull --ignore-pull-failures
 docker-compose -f docker-compose.yml -p home-lab up -d
 if [ $install_nextcloud = 'true' ]; then
     echo -e "${blue}updating containers used in stack 'home-nextcloud'${reset}"
-    docker-compose -f docker-compose-nextcloud.yml -p home-nextcloud pull
+    docker-compose -f docker-compose-nextcloud.yml -p home-nextcloud pull --ignore-pull-failures
     docker-compose -f docker-compose-nextcloud.yml -p home-nextcloud up -d
 fi
 docker image prune -f
