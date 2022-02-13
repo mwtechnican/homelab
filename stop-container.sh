1b0e2b9834acf4617f032716b0ac617522eb8b02
mwtechnican <moritz.wiedmann@outlook.de>
2022-02-13 20:39:50 +0100

    bug fix

--- stop-container.sh
+++ stop-container.sh
@@ -4,10 +4,8 @@ echo -e "loading variables '.env'"
 
 echo -e "stopping containers used in stack 'home-lab'"
 docker-compose -f docker-compose.yml -p home-lab down
-docker-compose -f docker-compose-nextcloud.yml -p home-nextcloud down
 if [ $install_nextcloud = 'true' ]; then
     echo -e "stopping containers used in stack 'home-nextcloud'"
     docker-compose -f docker-compose.yml -p home-lab down
-    docker-compose -f docker-compose-nextcloud.yml -p home-nextcloud down
 fi
 echo ""
