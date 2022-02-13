b58896b50e385963950950247e812972076d943e
mwtechnican <moritz.wiedmann@outlook.de>
2022-02-13 23:01:47 +0100

    added color

--- scripts/get_checkmk_image.sh
+++ scripts/get_checkmk_image.sh
@@ -4,14 +4,16 @@
 # Guide         : https://docs.checkmk.com/latest/en/introduction_docker.html
 # example link  : https://download.checkmk.com/checkmk/2.0.0p19/check-mk-free-docker-2.0.0p19.tar.gz
 ##################################################################
+blue=`tput setaf 4` # use color
+reset=`tput sgr0` # reset color
 
 checkmk_edition=$1
 checkmk_version=$2
 
 mkdir -p ./images
 
-if test -z "$(docker images -q checkmk/$(echo "${checkmk_edition%-docker}"):${checkmk_version})"; then
-    echo "downloading checkmk image - ${checkmk_edition}-${checkmk_version}"
-    wget https://download.checkmk.com/checkmk/${checkmk_version}/${checkmk_edition}-${checkmk_version}.tar.gz -O ./images/${checkmk_edition}-${checkmk_version}.tar.gz
-    docker load -i ./images/${checkmk_edition}-${checkmk_version}.tar.gz
+if test -z "$(docker images -q checkmk/$(echo "${checkmk_edition}"):${checkmk_version})"; then
+    echo "${blue}downloading checkmk image - ${checkmk_edition}-${checkmk_version}${reset}"
+    wget https://download.checkmk.com/checkmk/${checkmk_version}/${checkmk_edition}-docker-${checkmk_version}.tar.gz -O ./images/${checkmk_edition}-docker-${checkmk_version}.tar.gz
+    docker load -i ./images/${checkmk_edition}-docker-${checkmk_version}.tar.gz
 fi
