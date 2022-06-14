76434d7c325f093b78480bdec22e611c619ed127
mwtechnican <moritz.wiedmann@outlook.de>
2022-06-14 20:24:08 +0200

    taiga implementation

--- update-container.sh
+++ update-container.sh
@@ -4,7 +4,11 @@ echo "loading checkmk image"
 # bash scripts/get_checkmk_image.sh $checkmk_edition $checkmk_version
 
 echo "updating containers used in stack 'home-lab'"
-docker compose -f docker-compose.yml -p home-lab pull --ignore-pull-failures
+docker compose -f docker-compose.yml -p home-lab pull
+
+# taiga
+docker exec -it taiga-back python manage.py migrate
+docker exec -it taiga-back python manage.py collectstatic
 
 echo "checkmk agent musted be updated in the checkmk container"
 echo ""
