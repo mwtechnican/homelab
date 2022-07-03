778dabd96652dede1e5c6075c5747f1f4fab5ec7
mwtechnican <moritz.wiedmann@outlook.de>
2022-07-03 22:05:49 +0200

    update to use better checkmk update process

--- scripts/install_check-mk-agent.sh
+++ scripts/install_check-mk-agent.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 FILENAME=$(ls data/checkmk/cmk/ | grep check-mk-agent)
-docker exec --workdir /omd/sites/cmk checkmk dpkg --install $FILENAME
+docker exec --workdir /omd/sites/cmk checkmk dpkg --install $FILENAME > null
+
+# Agent Updates
+docker exec -it -u root checkmk cmk-update-agent register -s checkmk.wiedmann-tech.de -i cmk -H checkmk.wiedmann-tech.de -p https -U cmkadmin -P '9C[Z92=$C&%dZG8/M96m' -v > null
+docker exec -it -u root checkmk cmk-update-agent -v > null
