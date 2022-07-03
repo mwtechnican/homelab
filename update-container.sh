1f3737967c254e3cb59464a59d2944ff2b03380c
mwtechnican <moritz.wiedmann@outlook.de>
2022-07-03 22:06:12 +0200

    removed obsolete code

--- update-container.sh
+++ update-container.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 . .env
-echo "loading checkmk image"
-# bash scripts/get_checkmk_image.sh $checkmk_edition $checkmk_version
 
 echo "updating containers used in stack 'home-lab'"
 docker compose -f docker-compose.yml -p home-lab pull
-
-# taiga
-docker exec -it taiga-back python manage.py migrate
-docker exec -it taiga-back python manage.py collectstatic
-
-echo "checkmk agent musted be updated in the checkmk container"
-echo ""
